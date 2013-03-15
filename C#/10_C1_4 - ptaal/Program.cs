e277479824b5b1d0f2f37c94f99794dd0b600137
Glenn Latomme <glenn.latomme@gmail.com>
2013-03-15 17:46:36 +0100

    Perparing for next one

---
+++ C#/10_C1_4 - ptaal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_C1_4___ptaal {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
