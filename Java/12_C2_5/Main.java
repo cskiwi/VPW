00eae9ddecfd1dc8b4549fb3d31b587d66cd7386
thgh <thoogh@gmail.com>
2013-03-15 15:55:03 +0100

    nu werkt het

--- Java/12_C2_5/Main.java
+++ Java/12_C2_5/Main.java
@@ -1,62 +1,66 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
-
 public class Main {
-	static String[] grepen;
+	static String[] lower;
+	static String[] normal;
+	static List<String> check;
 	static int max;
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		// Opgaven
 		int opgaven = sc.nextInt();
+		String output = "";
 		for (int o = 0; o < opgaven; o++) {
-			
-			//input
+
+			// input
 			int aantalWoorden = sc.nextInt();
-			String[] woorden=new String[aantalWoorden];
+			String[] woorden = new String[aantalWoorden];
 			for (int w = 0; w < aantalWoorden; w++) {
-				woorden[w]=sc.next().toLowerCase();
+				woorden[w] = sc.next().toLowerCase();
 			}
 			int aantalGrepen = sc.nextInt();
-			grepen=new String[aantalGrepen];
-			max=0;
+			lower = new String[aantalGrepen];
+			normal = new String[aantalGrepen];
+			max = 0;
 			for (int g = 0; g < aantalGrepen; g++) {
-				grepen[g]=sc.next().toLowerCase();
-				max=max>grepen[g].length()?max:grepen[g].length();
+				normal[g] = sc.next();
+				lower[g] = normal[g].toLowerCase();
+				max = max > lower[g].length() ? max : lower[g].length();
 			}
-			System.out.println(max);
+			check=Arrays.asList(lower);
 			for (int w = 0; w < aantalWoorden; w++) {
-				System.out.println(woorden[w]);
-			}
-			for (int g = 0; g < aantalGrepen; g++) {
-				System.out.println(grepen[g]);
-			}
-
-			for (int w = 0; w < aantalWoorden; w++) {
-				System.out.println(solve(woorden[w]));
+				String temp= solve(woorden[w]);
+				if(temp==null)output+="nee";
+				else{output+=temp;}
+				if(o<opgaven-1||w<aantalWoorden-1)output+="\n";
 			}
 		}
+		System.out.print(output);
 		sc.close();
 	}
 
 	private static String solve(String rest) {
-		if(rest.length()==0)return null;
-		for (int i = 0;i < max && i< rest.length(); i++) {
-			String doing=rest.substring(0,i+1);
-			int p=Arrays.asList(grepen).indexOf(doing);
-			if(p!=-1){
-				String todo=rest.substring(i+1);
-				String solved=solve(todo);
-				System.out.println("todo "+todo);
-				if(solved!=null){
-					System.out.println("solved "+solved);
-					return doing+"-"+solved;
+		int len = rest.length();
+		if (len == 0)
+			return "";
+		for (int i = 0; i < max && i < len; i++) {
+			String doing = rest.substring(0, i + 1);
+			int p = check.indexOf(doing);
+			if (p != -1) {
+				String todo = rest.substring(i + 1);
+				String solved = solve(todo);
+				if (solved != null) {
+					if (solved.length() == 0)
+						return normal[p];
+					return normal[p] + "-" + solved;
 				}
 			}
 		}
-		
+
 		return null;
 	}
 }
